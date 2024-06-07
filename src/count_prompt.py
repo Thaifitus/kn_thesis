711751820ecee2648901bb11679de3f86b63d9be
Thaifitus <nththai20@clc.fitus.edu.vn>
2024-06-07 14:11:03 +0700

    Add result of 3_modify_activation Executed with self rel bag, base Refer to /Drive/Thesis/code_research/results/figs for two result .pdf files

--- src/count_prompt.py
+++ src/count_prompt.py
@@ -1,7 +1,7 @@
 '''
 Chương trình đếm và lưu trữ số lượng template prompt và số lượng prompt của mỗi relation tại tập tin .txt. Tập tin đầu vào và đầu ra tương ứng của chương trình bao gồm:
-Input: tập tin "data_all_allbags.json".
-Output: một tập tin chứa .txt chứa tên các relation, số lượng template và số lượng prompt tương ứng tại "/results/".
+Input: tập tin "data_all_allbags.json"
+Output: một tập tin .txt chứa tên các relation, số lượng template và số lượng prompt tương ứng tại "/results/"
 '''
 
 import json, jsonlines
@@ -12,7 +12,7 @@ total_template = 0
 
 def count_prompt(data_directory):
 	'''
-	(str) -> text (.txt) file in "/results/"
+	(str) -> text (.txt) file
 
 	Description:
 	data_directory: e.g. '../data/PARAREL/data_all_allbags.json'
@@ -22,6 +22,7 @@ def count_prompt(data_directory):
 
 	global total_prompt, total_template
 
+	# Read data
 	if os.path.exists(data_directory): # read file if data file exists
 		with open(data_directory, 'r') as f:
 			eval_bag_list_perrel = json.load(f)
@@ -29,25 +30,25 @@ def count_prompt(data_directory):
 	 	print("Data does not exist")
 	 	return
 
-	counts = {}
+	# Counting
+	count_record = {}
 	with jsonlines.open('../results/count_prompt.txt', 'w') as fw:
 		# loop considers each relation
 		for relation, eval_bag_list in eval_bag_list_perrel.items():
 			# Initialize record
-			counts[relation] = {
+			count_record[relation] = {
 				'num of template (bag)': 0,
 				'num of prompt': 0
 			}
-			counts[relation]['num of template (bag)'] += len(eval_bag_list)
+			# Count template prompt
+			count_record[relation]['num of template (bag)'] += len(eval_bag_list)
 			total_template += len(eval_bag_list)
-
-			# loop considers each template prompt
+			# Count prompt
 			for bag_idx, eval_bag in enumerate(eval_bag_list):
-				counts[relation]['num of prompt'] += len(eval_bag)
+				count_record[relation]['num of prompt'] += len(eval_bag)
 				total_prompt += len(eval_bag)
-			
 			# Write file for each relation
-			fw.write(f'{relation} : {counts[relation]}')
+			fw.write(f'{relation} : {count_record[relation]}')
 
 		# Write total number of template and prompt to file
 		fw.write(f'Number of templates: {total_template} - Number of prompts: {total_prompt}')
